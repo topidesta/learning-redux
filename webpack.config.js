7f1061e962ebbe48c4d56801338c85f3b6067843
Desta F Muchamad <desta.08b@gmail.com>
2020-06-06 14:41:14 +0700

    Added Webpack Dev Server Live Reload on the web ...

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,21 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        loader: "babel-loader"
-      }
-    ]
+        loader: "babel-loader",
+      },
+    ],
   },
-  devtool: "cheap-module-eval-source-map"
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+  },
+  devtool: "cheap-module-eval-source-map",
 };
