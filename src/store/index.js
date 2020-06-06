28d87c46494fad22e22fd9a946851b69f56b0016
Desta F Muchamad <desta.08b@gmail.com>
2020-06-06 15:08:51 +0700

    Create Middleware style #2

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,11 @@ import { createStore } from "redux";
 
 // create midlleware for logging
 // higher order function
-const consoleMessages = function (store) {
-  return function (next) {
-    return function (action) {};
-  };
+// 3 arrow function 
+const consoleMessages = (store) => (next) => (action) {
+  let result;
+  result = next(action)
+  return result;
 };
 
 export default (initialState = {}) => {
